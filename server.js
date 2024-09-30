80720f839395cf0bce25608baf721a131f93f6ab
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-09-30 18:28:39 -0300

    autentication

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
-const corsOptions = require("./config/corsOptions")
+const path = require('path');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 3500;
 
-app.use('/employees', require('./routes/api/employees'));
 
-app.use(cors(corsOptions))
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+
+// routes
+app.use('/register', require('./routes/register'));
+app.use('/employees', require('./routes/api/employees'));
+app.use('/auth', require('./routes/auth'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
