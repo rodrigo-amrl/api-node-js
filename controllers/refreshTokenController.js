5efabc86ecbac58fc0629892e33dd1aad1907283
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2024-09-30 22:35:04 -0300

    roles

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -10,14 +10,20 @@ const handleRefreshToken = async (req, res) => {
     console.log(cookies)
     if (!cookies?.jwt) return res.sendStatus(401)
 
-    console.log(cookies.jwt)
     const refreshToken = cookies.jwt
     const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken)
     if (!foundUser) return res.sendStatus(403)
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
         if (err || foundUser.username !== decoded.username) return res.sendStatus(403)
-        const accessToken = jwt.sign({ username: decoded.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' })
+        const roles = Object.values(foundUser.roles)
+        const accessToken = jwt.sign(
+            {
+                "UserInfo": {
+                    "username": decoded.username,
+                    "roles": roles
+                }
+            }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' })
         res.json({ accessToken })
     })
 
--- routes/api/employees.js
+++ routes/api/employees.js
@@ -2,12 +2,14 @@ const express = require('express')
 const router = express.Router()
 const data = {}
 const employeeController = require('../../controllers/employeesController')
-
+const ROLES_LIST = require('../../config/roles_list')
+const verifyRoles = require('../../middleware/verifyRoles')
 data.employees = require('../../model/employees.json')
+
 router.route('/')
-    .get(employeeController.getAllEmployees)
-    .post(employeeController.createNewEmployee)
-    .put(employeeController.updateEmployee)
+    .get(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeeController.getAllEmployees)
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeeController.createNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeeController.updateEmployee)
 router.route("/:id")
     .get(employeeController.getEmployee)
 module.exports = router;
